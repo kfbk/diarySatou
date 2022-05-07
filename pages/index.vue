6e16115413b9068b017258ab7b33f632276c4a19
satou1497 <satou1497@gmail.com>
2022-05-07 11:40:58 +0900

    220507

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
     <ul>
       <li v-for="article in article" :key="article.slug">
         <nuxt-link :to="'/' + article.slug">
-          <!-- <p>{{ article.title }} / {{ formatDate(article.createdAt)}}</p> -->
-          <p>{{ article.title }} / {{ article.createdAt}}</p>
+          <p>{{ article.title }} / {{ formatDate(article.createdAt)}}</p>
+          <!-- <p>{{ article.title }} / {{ article.createdAt}}</p> -->
         </nuxt-link>
       </li>
     </ul>
